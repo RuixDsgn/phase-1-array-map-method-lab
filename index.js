82c9232925bdeee8f4c29d42d9f63a6170201741
Carla <ruixdsgn@gmail.com>
2023-05-11 16:41:29 -0700

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,81 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// const splitTutorials = tutorials.map((title) => title.split(" "))
+// const capitalizeSplitTutorials = splitTutorials.map((title) => title.map((word) => word.charAt(0).toUpperCase() + word.slice(1)))
+// const joinedCapitalizeSplitTutorials = capitalizeSplitTutorials.map((tutorialTitle) => tutorialTitle.join(" "))
+
+const titleCased = () => { 
+  const result = tutorials.map((tutorial) => { 
+    var capitalizeText = tutorial.split(" ");
+    for(let i=0; i<capitalizeText.length; i++){
+      capitalizeText[i] = capitalizeText[i].charAt(0).toUpperCase() + capitalizeText[i].slice(1);    
+      }
+    return capitalizeText.join(" ");
+   })    
+   return result
+  }
+  console.log(titleCased());
+
+
+// const titleCased = () => {
+//   const splitTutorials = () => {
+//     tutorials.map((title) => {
+//       title.split(" ")
+//     })
+//   }
+//   const capitalizeSplitTutorials = () => {
+//     splitTutorials.map((title) => {
+//       title.map((word) => {
+//         word.charAt(0).toUpperCase() + word.slice(1)
+//       })
+//     }) 
+//   }
+//   const joinedCapitalizeSplitTutorials = () => {
+//     capitalizeSplitTutorials.map((tutorialTitle) => {
+//       tutorialTitle.join(" ")
+//     })
+//     return joinedCapitalizeSplitTutorials
+//   }
+//   return titleCased
+// }
+
+// const splitTutorials = () => {
+//   tutorials.map((title) =>  {
+//     title.split(" ")
+//   })
+// }
+
+// const capitalizeSplitTutorials = () => {splitTutorials.map((title) => {
+//   title.map((word) => {word.charAt(0).toUpperCase() + word.slice(1)
+//   })
+// })
+// }
+
+// const titleCased = () => {capitalizeSplitTutorials.map((tutorialTitle) => {
+//   tutorialTitle.join(" ")
+// })
+// return titleCased
+// }
+
+
+   
+
+// Create a new array in which every first letter of the word in the array of sentences should be capitalized
+  //split each title into its own array with .split() => 'what does the this keyword mean?' will be ['what', 'does', 'the', 'this', 'keyword', 'mean?']
+     // const splitTitle = tutorials.split(" ") 
+    
+       
+
+  // how can we change the first letter of a word to be upper case and return that modified letter along with the rest of the name?
+    // use 
+  // we know that we can target a specific letter of a word by using .charAt() 
+  // we know that we can use .toUpperCase() to make sure that target word, or string for this case if we combine .toUpperCase with .chatAt() 
+  //    will be changes to an upper case
+  // using .split() splits a string into an array of substring
+  // using .join() creates a new string by concatenating all of elements in an array
+
+  
+
+  // use .map to iterate through the newly splitted words
+  //  
